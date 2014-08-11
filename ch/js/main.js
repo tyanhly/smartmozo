844d16a72693e2347b533bb9f521134cdbdc54d6
tyanhly <tyanhly@yahoo.com>
2014-08-11 13:45:05 +0700

    Chinese language

---
+++ ch/js/main.js
@@ -0,0 +1,301 @@
+
+constants = {};
+unit = 200;
+constants.step1      = unit;
+constants.step2      = constants.step1 + unit/2;
+constants.step3      = constants.step2 + unit/2;
+constants.step4      = constants.step3 + unit;
+constants.step5      = constants.step4 + unit;
+constants.step6      = constants.step5 + unit/4;
+constants.step7      = constants.step6 + unit/4;
+constants.step8      = constants.step7 + unit/2;
+constants.step9      = constants.step8 + unit/2;
+
+constants.step10      = constants.step9 + unit;
+constants.step11      = constants.step10 + unit;
+constants.step12      = constants.step11 + unit;
+//constants.step13      = constants.step12 + unit; 
+constants.step14      = constants.step12 + unit;
+
+constants.step145      = constants.step14 + unit;
+
+constants.step15      = constants.step145 + 2*unit;
+constants.step16      = constants.step14 + unit;
+constants.step17      = constants.step16 + unit;
+constants.step18      = constants.step17 + unit;
+constants.step19      = constants.step18 + unit;
+
+
+
+constants.step20      = constants.step19 + unit;
+constants.step201     = constants.step20 + unit;
+constants.step21      = constants.step201 +  unit;
+
+constants.step22      = constants.step21 + unit;
+constants.step221      = constants.step22 + unit;
+constants.step2211      = constants.step221 + unit;
+constants.step2212      = constants.step2211 + unit;
+constants.step2213      = constants.step2212 + unit/2;
+constants.step2214      = constants.step2213 + unit/2;
+constants.step2215      = constants.step2214 + unit/2;
+constants.step2216      = constants.step2215 + unit/2;
+constants.step2217      = constants.step2216 + unit/2;
+
+
+constants.step222      = constants.step2217 + unit;
+constants.step223      = constants.step222 + unit;
+constants.step224      = constants.step223 + unit;
+constants.step225      = constants.step224 + unit;
+
+constants.step23      = constants.step225 + unit;
+constants.step24      = constants.step23 + unit;
+constants.step25      = constants.step24 + unit;
+
+constants.step251      = constants.step25 + 3/2*unit;
+
+constants.step26      = constants.step251 + unit;
+constants.step27      = constants.step26 + unit;
+constants.step271      = constants.step27 + 3/2*unit;
+constants.step28      = constants.step271 + unit;
+constants.step29      = constants.step28 + unit;
+constants.step291      = constants.step29 + 3/2*unit;
+
+
+
+constants.step30      = constants.step291 + unit;
+constants.step31      = constants.step30 + unit;
+constants.step311      = constants.step31 + 3/2*unit;
+
+constants.step32      = constants.step311 + unit;
+constants.step33      = constants.step32 + unit;
+constants.step331      = constants.step33 + 3/2*unit;
+
+constants.step34      = constants.step331 + unit;
+constants.step35      = constants.step34 + unit;
+constants.step351      = constants.step35 + 3/2*unit;
+
+constants.step36      = constants.step351 + unit;
+constants.step37      = constants.step36 + unit;
+constants.step371      = constants.step37 + 3/2*unit;
+
+constants.step38      = constants.step371 + unit;
+constants.step39      = constants.step38 + unit;
+constants.step391      = constants.step39 + 3/2*unit;
+
+
+
+constants.step40      = constants.step391 + unit;
+constants.step41      = constants.step40 + unit;
+constants.step411      = constants.step41 + 3/2*unit;
+
+constants.step42      = constants.step411 + unit;
+constants.step43      = constants.step42 + unit;
+constants.step431      = constants.step43 + 3/2*unit;
+
+constants.step44      = constants.step431 + unit;
+constants.step45      = constants.step44 + unit;
+constants.step451      = constants.step45 + unit;
+constants.step452      = constants.step451 + unit;
+
+constants.step46      = constants.step452 + unit;
+constants.step47      = constants.step46 + unit;
+
+constants.step48      = constants.step47 + unit;
+constants.step49      = constants.step48 + unit;
+constants.step491      = constants.step49 + unit/3;
+constants.step492      = constants.step491 + unit/3;
+constants.step493      = constants.step492 + unit/3;
+constants.step494      = constants.step493 + unit/3;
+
+constants.step50      = constants.step494 + unit/3;
+constants.step501      = constants.step50 + unit/3;
+constants.step502     = constants.step501 + unit/3;
+constants.step503      = constants.step502 + unit/3;
+
+constants.step51      = constants.step503 + unit/3;
+constants.step511      = constants.step51 + unit/3;
+constants.step512      = constants.step511 + unit/3;
+constants.step513      = constants.step512 + unit/3;
+constants.step514      = constants.step513 + unit/3;
+
+constants.step52      = constants.step513 + 2*unit;
+constants.step53      = constants.step52 + 3*unit;
+
+constants.step54      = constants.step53 + unit;
+
+constants.step541      = constants.step54 + unit/5;
+constants.step542      = constants.step541 + unit/5;
+constants.step543      = constants.step542 + unit/5;
+constants.step544      = constants.step543 + unit/5;
+constants.step545      = constants.step544 + unit/5;
+constants.step546      = constants.step545 + unit/5;
+
+constants.step55      = constants.step544 + unit;
+constants.step56      = constants.step55 + unit;
+constants.step561      = constants.step56 + unit;
+constants.step57      = constants.step561 + unit;
+constants.step58      = constants.step57 + unit;
+constants.step59      = constants.step58 + unit;
+
+
+
+constants.step60      = constants.step59 + unit;
+constants.step61      = constants.step60 + unit;
+constants.step62      = constants.step61 + unit;
+constants.step63      = constants.step62 + unit;
+constants.step64      = constants.step63 + unit;
+constants.step65      = constants.step64 + unit;
+constants.step66      = constants.step65 + unit;
+constants.step661      = constants.step66 + unit;
+constants.step67      = constants.step661 + unit;
+constants.step68      = constants.step67 + unit;
+constants.step69      = constants.step68 + unit;
+
+
+constants.step70      = constants.step69 + unit;
+constants.step71      = constants.step70 + unit;
+constants.step72      = constants.step71 + unit;
+constants.step73      = constants.step72 + unit;
+constants.step74      = constants.step73 + unit;
+constants.step75      = constants.step74 + unit;
+constants.step751      = constants.step75 + unit;
+constants.step76      = constants.step751 + unit;
+constants.step77      = constants.step76 + unit;
+constants.step78      = constants.step77 + unit;
+constants.step79      = constants.step78+ unit;
+
+
+constants.step80      = constants.step79 + unit;
+constants.step81      = constants.step80 + unit;
+constants.step82      = constants.step81 + unit;
+constants.step83      = constants.step82 + unit;
+constants.step831      = constants.step83 + unit;
+constants.step84      = constants.step831 + unit;
+constants.step85      = constants.step84 + unit;
+constants.step86      = constants.step85 + unit;
+constants.step87      = constants.step86 + unit;
+constants.step88      = constants.step87 + unit;
+constants.step89      = constants.step88 + unit;
+
+
+constants.step90      = constants.step89 + unit;
+constants.step91      = constants.step90 + unit;
+constants.step911      = constants.step91 + unit;
+constants.step92      = constants.step911 + unit;
+constants.step93      = constants.step92 + unit;
+constants.step94      = constants.step93 + unit;
+constants.step95      = constants.step94 + unit;
+constants.step96      = constants.step95 + unit;
+constants.step97      = constants.step96 + unit;
+constants.step98      = constants.step97 + unit;
+constants.step99      = constants.step98 + unit;
+
+constants.step991      = constants.step99 + unit;
+
+
+constants.step100      = constants.step991 + unit;
+constants.step101      = constants.step100 + unit;
+constants.step102      = constants.step101 + unit;
+constants.step103      = constants.step102 + unit;
+constants.step104      = constants.step103 + unit;
+constants.step105      = constants.step104 + unit;
+constants.step106      = constants.step105 + unit;
+constants.step107      = constants.step106 + unit;
+constants.step1071      = constants.step107 + unit;
+constants.step108      = constants.step1071 + unit;
+constants.step109      = constants.step108 + unit;
+
+
+
+constants.step110      = constants.step109 + unit;
+constants.step111      = constants.step110 + unit;
+constants.step112      = constants.step111 + unit;
+constants.step113      = constants.step112 + unit;
+constants.step114      = constants.step113 + unit;
+constants.step115      = constants.step114 + unit;
+constants.step116      = constants.step115 + unit;
+constants.step1161      = constants.step116 + unit;
+constants.step117      = constants.step116 + unit;
+constants.step118      = constants.step117 + unit;
+constants.step119      = constants.step118 + unit;
+
+
+
+var s = skrollr.init(
+{
+	constants: constants
+});
+
+
+
+
+
+
+
+/**********************************auto run********************************/
+autoDistance = 30;
+autoInterval = 100;
+var i;
+
+
+$("#scrollPlay").on('click', function(){
+	if($(this).hasClass('pause')){
+		clearInterval(i);
+		$(this).removeClass('pause');
+	}else{
+		i = setInterval(function(){		
+			_currentScrollTop = s.getScrollTop();
+			s.setScrollTop(_currentScrollTop+autoDistance, true);
+		},autoInterval);
+		$(this).addClass('pause');
+	}
+});
+
+/**************************scroll*******************************************/
+
+// _isScroll=false;
+// _scrollTop = s.getScrollTop();
+// setInterval(function(){		
+// 	_currentScrollTop = s.getScrollTop();
+
+// 	if(_currentScrollTop !== _scrollTop){
+// 		_isScroll=true;
+// 	}else{
+// 		_isScroll=false;
+// 	}
+
+// 	if(_isScroll){
+// 		$("#scroll").css({"opacity":"0"});
+// 	}else{
+
+// 		$("#scroll").css({"opacity":"1" });
+// 	}
+
+// 	_scrollTop = _currentScrollTop;
+// 	// alert(_currentScrollTop);
+// },900);
+
+/**************************************mouse auto*************************/
+// var _isAutoRun = true;
+// $(document).on('click taphold',function(e){
+
+// 	if(_isAutoRun){
+// 		i = setInterval(function(){		
+// 			_currentScrollTop = s.getScrollTop();
+// 			s.setScrollTop(_currentScrollTop+autoDistance, true);
+
+			
+// 		},autoInterval);
+// 		_isAutoRun=false;
+
+// 		$('html').addClass('pause');
+
+// 	}else{
+// 		clearInterval(i);
+
+// 		_isAutoRun=true;
+
+// 		$('html').removeClass('pause');
+// 	}
+
+// });
