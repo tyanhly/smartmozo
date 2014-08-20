2c6effba8a8d949ce8a4ce5f7c10d38c40d2da21
tyanhly <tyanhly@yahoo.com>
2014-08-20 13:22:14 +0700

    Edit new font and copyright css

--- js/main.js
+++ js/main.js
@@ -405,3 +405,24 @@ function down(){
     	// s.setScrollTop(screen1Top);
     }
 }
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
